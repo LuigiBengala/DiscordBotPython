719f110d70ceafa51a006da2de02c045fc6d455e
LuigiBengala <121065602+LuigiBengala@users.noreply.github.com>
2023-09-30 02:32:00 +0100

    Code optimization

--- main.py
+++ main.py
@@ -9,25 +9,39 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+async def sendImage(ctx, image):
+    await ctx.send(file=nextcord.File(f'./assets/images/{image}.jpg')) # Choose the image this command will send
+    
 @bot.command(name = 'imagetest')
-async def SendImage(ctx):
-    await ctx.send(file=nextcord.File('./assets/images/imageTest.jpg')) # Choose the image this command will send
+async def image(ctx):
+    await sendImage(ctx, 'imageTest')
 
+@bot.command(name = 'imagetest')
+async def image(ctx):
+  await SendImage(ctx, 'imageTest')
+  
+    
+async def sendMessage(ctx, message):
+    await ctx.send(message) # Choose the message this command will send
+    
 @bot.command(name = 'textTest')
-async def SendMessage(ctx):
-    await ctx.send('this message was sucessufuly sent') # Choose the message this command will send
+async def message(ctx):
+    await sendMessage(ctx, 'textMessage')
  
-@bot.command(name = 'textAudio')
-async def join(ctx):
+async def playAudio(ctx, name):
     if (ctx.author.voice):
       channel = ctx.message.author.voice.channel
       voice = await channel.connect()
-      source = FFmpegPCMAudio('./assets/audios/samsung.wav') # Path to your audio file
+      source = FFmpegPCMAudio('./assets/audios/{name}.mp3') # Path to your audio file
       voice.play(source)
-      await asyncio.sleep(2) # Set the time to sleep the same as the audio you want to play
+      await asyncio.sleep(source.duration) # Set the time to sleep the same as the audio you want to play
       await ctx.guild.voice_client.disconnect()
     else:
       await ctx.send("You are not connected to a voice channel.")
+      
+@bot.command(name = 'audioTest')
+async def sendAudio(ctx):
+    await playAudio(ctx, 'audioTest') # Choose the audio this command will play
 
 if __name__ == '__main__':
     bot.run('Your token')
